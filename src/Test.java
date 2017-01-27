14f8e5cc6a584ed874ea4b9c4d935efa8934b413
TheSatox <te14.oscar.eriksson@umea.nti.se>
2017-01-27 09:40:24 +0100

    Added some code

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by eros14 on 2017-01-20.
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-        URL oracle = new URL("http://www.oracle.com/");
+        URL oracle = new URL("https://www.oracle.com/index.html");
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(oracle.openStream()));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null)
             System.out.println(inputLine);
+        inputLine.contains("<a");
         in.close();
+
     }
 }
 
